58080f58b14e6d27bd45b419281415e63614b5d7
Salomon Gutierrez <salomon19982009@hotmail.com>
2023-11-13 13:51:07 -0500

    Ejer 4 +extra

--- index.js
+++ index.js
@@ -38,15 +38,78 @@ botonReiniciarTexto.addEventListener("click", (event) => {
 //Ejercicio 3
 
 const botonCalcularIMC = document.getElementById("botonCalcularIMC")
-const resultadoIMC = document.getElementById("resultadoIMC")
-let peso, estatura
 
 botonCalcularIMC.addEventListener("click", (event) => {
     event.preventDefault()
-    peso = document.getElementById("peso").value / 10
-    estatura = document.getElementById("estatura").value
+    const resultadoIMC = document.getElementById("resultadoIMC")
+    const peso = document.getElementById("peso").value
+    const estatura = document.getElementById("estatura").value / 100
     resultadoIMC.value = peso / estatura ** 2
     console.log(resultadoIMC.value)
 })
 
 //Ejercicio 4
+
+const botonConvertirDivisa = document.getElementById("botonConvertirDivisa")
+const botonReiniciarDivisa = document.getElementById("botonReiniciarDivisa")
+const localADolar = document.getElementById("localADolar")
+const dolarALocal = document.getElementById("dolarALocal")
+const formulario = document.getElementById("convertirDivisa")
+const conversion = 4028.65
+
+const valorIngresado = document.getElementById("monedaLocal")
+const resultado = document.getElementById("dolares")
+
+
+
+localADolar.addEventListener("click", () => {
+    formulario.innerHTML = `<label class="font-bold" for="monedaLocal" id="monedaLocalLabel">Moneda local (Peso Colombiano)
+    <label class="italic font-normal" for="monedaLocal"><br>sin comas ni puntos</label>
+</label>
+<input class="text-[#001427] p-2 text-center" type="number" name="monedaLocal" id="moneda">
+
+<label class="font-bold" for="dolares" id="dolaresLabel">Dólares
+    <label class="italic font-normal" for="dolares"><br>sin comas ni puntos</label>
+</label>
+<input class="text-[#001427] p-2 text-center" type="number" name="dolares" id="resultado" readonly>`
+})
+
+
+dolarALocal.addEventListener("click", () => {
+    formulario.innerHTML = `<label class="font-bold" for="dolares" id="dolaresLabel">Dólares
+    <label class="italic font-normal" for="dolares"><br>sin comas ni puntos</label>
+</label>
+<input class="text-[#001427] p-2 text-center" type="number" name="dolares" id="moneda">
+
+<label class="font-bold" for="monedaLocal" id="monedaLocalLabel">Moneda local (Peso Colombiano)
+    <label class="italic font-normal" for="monedaLocal"><br>sin comas ni puntos</label>
+</label>
+<input class="text-[#001427] p-2 text-center" type="number" name="monedaLocal" id="resultado" readonly>`
+})
+
+botonConvertirDivisa.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    const localADolar = document.getElementById("localADolar")
+    const dolarALocal = document.getElementById("dolarALocal")
+    const conversion = 4028.65
+
+    const resultado = document.getElementById("resultado")
+    if (localADolar.checked) {
+        monLocal = document.getElementById("moneda").value
+        resultado.value = monLocal / conversion
+    }
+    else if (dolarALocal.checked) {
+        dolar = document.getElementById("moneda").value
+        resultado.value = dolar * conversion
+    }
+})
+
+
+botonReiniciarDivisa.addEventListener("click", (event) => {
+    event.preventDefault()
+    const valorIngresado = document.getElementById("moneda")
+    const resultado = document.getElementById("resultado")
+    valorIngresado.value = 0
+    resultado.value = 0
+})
